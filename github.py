e0bd9cc482c44c73cd2da4fab1d1809151310f16
Johannes Mittendorfer <joushx@users.noreply.github.com>
2013-10-15 15:27:19 +0200

    Create github.py

---
+++ github.py
@@ -0,0 +1,11 @@
+from oauth import OAuth, OAuthUrl
+
+class Github(OAuth):
+
+	def __init__(self, consumer_key, consumer_secret):
+		self.request_token_url = "https://github.com/login/oauth/request_token"
+		self.authorize_url = "https://github.com/login/oauth/authorize"
+		self.access_token_url = "https://github.com/login/oauth/access_token"
+
+		self.consumer_key = consumer_key
+		self.consumer_secret = consumer_secret
