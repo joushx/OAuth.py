7ea81c04038dbbfddc482a910150285ff665cdbe
Johannes Mittendorfer <joushx@users.noreply.github.com>
2013-10-15 15:22:50 +0200

    initial commit

---
+++ example.py
@@ -0,0 +1,10 @@
+o = Twitter("your-consumer-key","your-consumer-secret")
+o.getRequestToken()
+print o.getAuthorizeUrl()
+verifier = raw_input("Verifier:")
+o.getAccessToken(verifier)
+
+while(1 == 1):
+	m = raw_input("Method:")
+	u = raw_input("URL:")
+	print o.get(m,u)
